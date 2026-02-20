40317dd6ad540c783fdebaa5816ffc882912ae2f
Anuradha-Folane <anufolane@gmail.com>
2026-02-20 22:16:49 +0530

    Enhance login tests with negative scenario
    Added a negative scenario test for invalid login credentials.

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,6 +1,6 @@
 from pages.login_page import LoginPage
 
-
+#login with correct credentials-Happy path scenario
 def test_valid_login(page):
     login_page = LoginPage(page)
 
@@ -8,3 +8,12 @@ def test_valid_login(page):
     login_page.login("tomsmith", "SuperSecretPassword!")
 
     assert "You logged into a secure area!" in login_page.get_message()
+
+#Login with incorrect credentials-Negative scenario
+def test_valid_login(page):
+    login_page = LoginPage(page)
+
+    login_page.open()
+    login_page.login("wronguser", "wrongpassword")
+
+    assert "secure area" in login_page.get_message()
